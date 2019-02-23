be639c665c51d46729ac4815c193673fcbbcc20c
Balint <korosibalint@yahoo.co.uk>
2019-02-23 14:19:49 +0100

    toggle done

---
+++ 05 - Flex Panel Gallery/app.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
