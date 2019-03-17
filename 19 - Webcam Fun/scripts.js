85285a59d217edc29e00d578421fafee418c8c84
Balint <korosibalint@yahoo.co.uk>
2019-03-17 14:09:17 +0100

    consider webcam project done

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,8 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+// let's just consider this project done
+// don't want to fuck with local server setup
